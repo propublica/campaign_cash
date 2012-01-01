cd67d895645bc985f706eaf1419a509401e8279c
Derek Willis <dwillis@gmail.com>
2012-01-01 11:26:48 -0500

    added tests for president class

--- test/campaign_cash/test_president.rb
+++ test/campaign_cash/test_president.rb
@@ -8,15 +8,16 @@ class TestCampaignCash::TestPresident < Test::Unit::TestCase
 		  reply = Base.invoke('2012/president/totals', {})
 			@results = reply['results']
 			@summaries = @results.map{|r| President.create_summary(r)}
+			@summary = @summaries.first
 		end
 		
 		should "return an array of objects of the President type" do
-			assert_kind_of(President, @summaries.first)
+			assert_kind_of(President, @summary)
 		end
 		
-		%w(name id party total_receipts).each do |attr|
+		%w(name total_contributions party total_receipts).each do |attr|
 			should "assign the value of the @#{attr} attribute from the '#{attr}' key in the hash" do
-				assert_equal(@summaries.first[attr], @summaries.first.send(attr))
+				assert_equal(@results.first[attr], @summary.send(attr))
 			end
 		end
   end
