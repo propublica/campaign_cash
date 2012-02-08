dbd245b0fe619ab5875d0b409cce051b79821125
Benjamin Jackson <bhjackson@gmail.com>
2012-02-08 10:42:47 -0500

    Changed party identifier for President model to match the ones used for Candidate and Committee model ("REP" and "DEM").

--- test/campaign_cash/test_president.rb
+++ test/campaign_cash/test_president.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TestCampaignCash::TestPresident < Test::Unit::TestCase
 	include CampaignCash
-	
+
 	context "President.summary" do
 		setup do
 		  reply = Base.invoke('2012/president/totals', {})
@@ -10,12 +10,12 @@ class TestCampaignCash::TestPresident < Test::Unit::TestCase
 			@summaries = @results.map{|r| President.create_summary(r)}
 			@summary = @summaries.first
 		end
-		
+
 		should "return an array of objects of the President type" do
 			assert_kind_of(President, @summary)
 		end
-		
-		%w(name total_contributions party total_receipts).each do |attr|
+
+		%w(name total_contributions total_receipts).each do |attr|
 			should "assign the value of the @#{attr} attribute from the '#{attr}' key in the hash" do
 				assert_equal(@results.first[attr], @summary.send(attr))
 			end
@@ -24,21 +24,28 @@ class TestCampaignCash::TestPresident < Test::Unit::TestCase
 		should "assign the office to 'president'" do
 			assert_equal('president', @summary.office)
 		end
-		
+
+		should "assign the party to 'REP' if the candidate is republican or 'DEM' if democrat" do
+		    party = @results.first['party']
+		    party = "REP" if party == "R"
+		    party = "DEM" if party == "D"
+		    assert_equal(party, @summary.party)
+		end
+
   end
-  
+
 	context "President.detail" do
 		setup do
 		  reply = Base.invoke('2012/president/candidates/C00496034', {})
 			@results = reply['results']
 			@detail = President.create_detail(@results.first)
 		end
-		
+
 		should "return an array of objects of the President type" do
 			assert_kind_of(President, @detail)
 		end
-		
-		%w(net_primary_contributions party total_refunds).each do |attr|
+
+		%w(net_primary_contributions total_refunds).each do |attr|
 			should "assign the value of the @#{attr} attribute from the '#{attr}' key in the hash" do
 				assert_equal(@results.first[attr], @detail.send(attr))
 			end
@@ -47,5 +54,12 @@ class TestCampaignCash::TestPresident < Test::Unit::TestCase
 		should "assign the office to 'president'" do
 			assert_equal('president', @detail.office)
 		end
-  end
+
+		should "assign the party to 'REP' if the candidate is republican or 'DEM' if democrat" do
+		    party = @results.first['party']
+		    party = "REP" if party == "R"
+		    party = "DEM" if party == "D"
+		    assert_equal(party, @detail.party)
+		end
+    end
 end
